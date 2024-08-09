6f304d3b632ba41b6fc6d30bc075d779ecb60eee
teenage1ove <kirillstars7@gmail.com>
2024-08-09 11:55:33 +0300

    Update get req

--- src/components/Articles/Articles.tsx
+++ src/components/Articles/Articles.tsx
@@ -1,28 +1,26 @@
 "use client"
 
+import { IResponseArticles } from '@/types'
 import { useQuery } from '@tanstack/react-query'
 import { NextPage } from 'next'
+import { BiLoaderAlt } from 'react-icons/bi'
 
-interface IArcticle {
-  id: number
-  title: string
-  image: string
-  link: string
-  level: string
-  description: string
-}
 
 interface Props {}
 
-const BASE_URL = 'http://localhost:3001/articles'
-
+const BASE_URL = 'https://api.jsonbin.io/v3/b/66b5d187e41b4d34e41def2b'
+const headers = new Headers()
+headers.append('X-Master-Key', '$2a$10$ObxR0lU04NQD2QS8xGRxRualGHB3tBYxKvv2HjysNigGUCAC/BoWC')
 const  getArticles = async() => {
-  const data = await fetch(BASE_URL).then(res => res.json())
+  const data = await fetch(BASE_URL, {
+    method: 'GET',
+    headers
+  }).then(res => res.json())
   return data
 }
 
 const Articles: NextPage<Props> = ({}) => {
-  const {data, isLoading, isError} = useQuery<IArcticle[]>({
+  const {data, isLoading, isError} = useQuery<IResponseArticles>({
     queryKey: ['articles'],
     queryFn: getArticles
   })
@@ -35,9 +33,9 @@ const Articles: NextPage<Props> = ({}) => {
 
     <div className='mt-10'>
         {isError && <div className='flex justify-center items-center mt-10 text-red-600'>Произошла ошибка</div>}
-        {isLoading ? <div className='flex justify-center items-center mt-10'>Загрузка схем...</div> : (
+        {isLoading ? <div className='flex justify-center items-center mt-10 animate-spin'><BiLoaderAlt /></div> : (
           <div className='flex gap-x-5 gap-y-10 items-center justify-center flex-wrap'>
-            {data?.map(item => (
+            {data?.record?.articles.map(item => (
             <a href={item.link} target='_blank'>
               <div key={item.id} className='max-w-72 min-h-96 flex flex-col items-center border-stone-600 border-2 rounded-lg px-2 py-3 bg-gray-400 bg-opacity-25 transition-all hover:-rotate-1'>
                 <h3 className='text-center font-bold text-xl mb-2'>{item.title}</h3>
